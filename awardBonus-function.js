119505081aa9c72349f909dc75935c55d436ae76
Emilia Burza <emila.burza@gmail.com>
2025-01-25 19:30:55 +0100

    Update console log in awardBonuses function

--- awardBonus-function.js
+++ awardBonus-function.js
@@ -23,13 +23,13 @@ function awardBonuses(){
     for (let i = 1; i <= 100; i++) {
 
         if ( i % 3 === 0 && i % 5 === 0 ) {
-            console.log('JACKPOT! 1 Million and a Yacht!');
+            console.log(i + ' - JACKPOT! 1 Million and a Yacht!');
         } else if ( i % 3 === 0 ) {
-            console.log('Vacation!');
+            console.log(i + ' - Vacation!');
         } else if ( i % 5 === 0 ) {
-            console.log('$100,000 bonus! ');
+            console.log(i + ' - $100,000 bonus! ');
         } else {
-            console.log(':(');
+            console.log(i + ' - :(');
         }
     }
 }
